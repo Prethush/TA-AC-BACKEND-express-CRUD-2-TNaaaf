0439a24c2e92ea1177bc62e56fc061b546ca4e82
Prethush <ktmirash49@gmail.com>
2021-07-07 17:59:22 +0530

    Blog application

--- block-BNaadl/blog/models/articles.js
+++ block-BNaadl/blog/models/articles.js
@@ -3,10 +3,10 @@ let Schema = mongoose.Schema;
 
 let articleSchema = new Schema({
     title: {type: String, required: true},
-    description: String,
+    description: {type: String, required: true},
     tags: [String],
     author: String,
-    likes: Number
+    likes: {type: Number, default: 0},
 }, {timestamps: true});
 
 module.exports = mongoose.model("Article", articleSchema);
